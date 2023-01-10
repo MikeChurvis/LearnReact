c52745fcee0493e36c5eaf766a8c87b670f36045
Mike Churvis <mikechurvis@gmail.com>
2023-01-10 18:26:00 -0500

    000: better demonstration of useEffect; useState on array; list rendering w/ loop

--- 000-counter/src/App.tsx
+++ 000-counter/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [numberHistory, setNumberHistory] = useState<number[]>([]);
 
   function incrementNumber() {
     setNumber((previousNumber) => previousNumber + 1);
@@ -11,8 +12,12 @@ function App() {
     setNumber((previousNumber) => previousNumber - 1);
   }
 
+  function addNumberToHistory(numberToAdd: number) {
+    setNumberHistory([numberToAdd, ...numberHistory]);
+  }
+
   useEffect(() => {
-    console.log(number);
+    addNumberToHistory(number);
   }, [number]);
 
   return (
@@ -20,6 +25,12 @@ function App() {
       <h1>Number: {number}</h1>
       <button onClick={decrementNumber}>Decrement</button>
       <button onClick={incrementNumber}>Increment</button>
+      <h2>Most recent numbers:</h2>
+      <ul>
+        {numberHistory.map((previousNumber, index, array) => {
+          return <li key={index}>{previousNumber}</li>;
+        })}
+      </ul>
     </main>
   );
 }
