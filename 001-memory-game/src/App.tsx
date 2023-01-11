dddb5d8de36ef32ed9de48a76a5055ec252b3c4d
Mike Churvis <mikechurvis@gmail.com>
2023-01-11 12:24:07 -0500

    001: update app
    Added inline styles. This will be replaced with something proper like Tailwind once the core functionality is in place.

--- 001-memory-game/src/App.tsx
+++ 001-memory-game/src/App.tsx
@@ -18,31 +18,42 @@ function App() {
   }
 
   return (
-    <>
-      <h1>Memory Game</h1>
-      <p>
-        Click a card to flip it over and see its number. Click a second card to
-        see its number too. If the two numbers are different, both cards will
-        flip back number-side down. If the numbers match, the cards will stay
-        number-side up.
-      </p>
-      <p>
-        <strong>When all cards are number-side up, you win!</strong>
-      </p>
-      <div id="memory-game-container">
-        {cardNumbers.map((value, index) => {
-          return (
-            <Card
-              key={index}
-              index={index}
-              value={value}
-              revealed={cardsRevealed[index]}
-              cardInteractionHandler={handleCardStateChange}
-            />
-          );
-        })}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ maxWidth: "50em" }}>
+        <h1>Memory Game</h1>
+        <p style={{ maxWidth: "72ch" }}>
+          Click a card to flip it over and see its number. Click a second card
+          to see its number too. If the two numbers are different, both cards
+          will flip back number-side down. If the numbers match, the cards will
+          stay number-side up.
+        </p>
+        <p>
+          <strong>When all cards are number-side up, you win!</strong>
+        </p>
+        <div
+          id="memory-game-container"
+          style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)" }}
+        >
+          {cardNumbers.map((value, index) => {
+            return (
+              <Card
+                key={index}
+                index={index}
+                value={value}
+                revealed={cardsRevealed[index]}
+                cardInteractionHandler={handleCardStateChange}
+              />
+            );
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
