6af12457c4048b980488a9c65c764e7ea6c63894
Mike Churvis <mikechurvis@gmail.com>
2023-01-13 08:21:38 -0500

    002: first test passes
    You can now add a todo item. The todo item persists to local storge.

--- 002-test-driven-todo-list/src/App.tsx
+++ 002-test-driven-todo-list/src/App.tsx
@@ -1,5 +1,41 @@
+import { FormEvent, useState } from "react";
+import { useLocalStorage } from "./hooks";
+
+type Todo = {
+  id: string;
+  title: string;
+};
+
 function App() {
-  return null;
+  const [todos, setTodos] = useLocalStorage<Todo[]>("react-todos", []);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  function handleCreateNewTodo(event: FormEvent) {
+    event.preventDefault();
+    setTodos([...todos, { id: Date.now().toString(), title: newTodoTitle }]);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleCreateNewTodo}>
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+        />
+      </form>
+      <ul>
+        {todos.map((todo: Todo) => {
+          return (
+            <li data-pw="todo-title" key={todo.id}>
+              {todo.title}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
